f12695482b0d93411c81e906974c9d059793ae1f
CHAITANYA POTARAJU <chaitanyaasu@gmail.com>
2024-03-25 06:26:05 -0700

    Adding scripts to understand vision transformers

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # ViT_Scratch
 Building vision transformer from scratch!
+Based on https://arxiv.org/abs/2010.11929
+
+## Usage
+
+Dependencies:
+- PyTorch 1.13.1 ([install instructions](https://pytorch.org/get-started/locally/))
+- torchvision 0.14.1 ([install instructions](https://pytorch.org/get-started/locally/))
+- matplotlib 3.7.1 to generate plots for model inspection
+
+The main class is `ViTForImageClassification` in utils/model_architecture.py, which contains the embedding layer, the transformer encoder, and the classification head. 
+
+The model config is defined as a python dictionary in `train.py`, you can experiment with different hyperparameters there (More room to improvement by adding parser module to capture this parameters from command line).
+
+```python
+config = {
+    "patch_size": 4,
+    "hidden_size": 48,
+    "num_hidden_layers": 4,
+    "num_attention_heads": 4,
+    "intermediate_size": 4 * 48,
+    "hidden_dropout_prob": 0.0,
+    "attention_probs_dropout_prob": 0.0,
+    "initializer_range": 0.02,
+    "image_size": 32,
+    "num_classes": 10,
+    "num_channels": 3,
+    "qkv_bias": True,
+}
+```
+
+The model is much smaller than the original ViT models from the paper (which has at least 12 layers and hidden size of 768) as I just want to illustrate how the model works rather than achieving state-of-the-art performance.
+
+There are other functions in utils/experiment.py to visualize the predictions and attention masks 
---
+++ train.py
@@ -0,0 +1,138 @@
+import os
+import torch
+from torch import nn, optim
+
+from utils.model_architecture import ViTForClassfication
+from utils.experiment import save_checkpoint, save_experiment, add_attention_tboard
+from utils.data_prep import prepare_data
+
+from torch.utils.tensorboard import SummaryWriter
+
+
+exp_name = 'vit_100_'
+batch_size = 32
+epochs = 1000
+lr = 1e-2
+save_model_every = 20
+writer = SummaryWriter(f'runs/{exp_name}')
+tboard = f'runs/{exp_name}/tb_images'
+
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+config = {
+    "patch_size": 4,  # Input image size: 32x32 -> 8x8 patches
+    "hidden_size": 384,
+    "num_hidden_layers": 6,
+    "num_attention_heads": 6,
+    "intermediate_size": 4 * 384, # 4 * hidden_size
+    "hidden_dropout_prob": 0.2,
+    "attention_probs_dropout_prob": 0.2,
+    "initializer_range": 0.02,
+    "image_size": 32,
+    "num_classes": 10, # num_classes of CIFAR10
+    "num_channels": 3,
+    "qkv_bias": True,
+    "use_faster_attention": True,
+}
+
+# These are not hard constraints, but are used to prevent misconfigurations
+assert config["hidden_size"] % config["num_attention_heads"] == 0
+assert config['intermediate_size'] == 4 * config['hidden_size']
+assert config['image_size'] % config['patch_size'] == 0
+
+
+class Trainer:
+    """
+    The simple trainer.
+    """
+
+    def __init__(self, model, optimizer, loss_fn, exp_name, device):
+        self.model = model.to(device)
+        self.optimizer = optimizer
+        self.loss_fn = loss_fn
+        self.exp_name = exp_name
+        self.device = device
+        self.best_val_loss = float('inf')
+
+    def train(self, trainloader, testloader, epochs, save_model_every_n_epochs=0):
+        """
+        Train the model for the specified number of epochs.
+        """
+        # Keep track of the losses and accuracies
+        train_losses, test_losses, accuracies = [], [], []
+        # Train the model
+        for i in range(epochs):
+            train_loss = self.train_epoch(trainloader)
+            accuracy, test_loss = self.evaluate(testloader)
+            train_losses.append(train_loss)
+            test_losses.append(test_loss)
+            accuracies.append(accuracy)
+            writer.add_scalar('Training loss', train_loss, i)
+            writer.add_scalar('Validation loss', test_loss, i)
+            writer.add_scalar('Accuracy', accuracy, i)
+            print(f"Epoch: {i+1}, Train loss: {train_loss:.4f}, Test loss: {test_loss:.4f}, Accuracy: {accuracy:.4f}")
+            if save_model_every_n_epochs > 0 and (i+1) % save_model_every_n_epochs == 0 and i+1 != epochs:
+                print('\tSave checkpoint at epoch', i+1)
+                save_checkpoint(self.exp_name, self.model, i+1)
+                pppp = os.path.join(tboard, f"attention_mask_{i}")
+                add_attention_tboard(self.model, output=pppp, device="cuda")
+                writer.add_image("Attention mask", pppp)
+            
+            if test_loss < self.best_val_loss:
+                print(f"Best model found at epoch: {i}")
+                save_checkpoint(self.exp_name, self.model, "best")
+                self.best_val_loss = test_loss
+
+            
+        # Save the experiment
+        save_experiment(self.exp_name, config, self.model, train_losses, test_losses, accuracies)
+
+    def train_epoch(self, trainloader):
+        """
+        Train the model for one epoch.
+        """
+        self.model.train()
+        total_loss = 0
+        for batch in trainloader:
+            batch = [t.to(self.device) for t in batch]
+            images, labels = batch
+            self.optimizer.zero_grad()
+            loss = self.loss_fn(self.model(images)[0], labels)
+            loss.backward()
+            self.optimizer.step()
+            total_loss += loss.item() * len(images)
+        return total_loss / len(trainloader.dataset)
+
+    @torch.no_grad()
+    def evaluate(self, testloader):
+        self.model.eval()
+        total_loss = 0
+        correct = 0
+        with torch.no_grad():
+            for batch in testloader:
+                batch = [t.to(self.device) for t in batch]
+                images, labels = batch
+                logits, _ = self.model(images)
+                loss = self.loss_fn(logits, labels)
+                total_loss += loss.item() * len(images)
+
+                predictions = torch.argmax(logits, dim=1)
+                correct += torch.sum(predictions == labels).item()
+        accuracy = correct / len(testloader.dataset)
+        avg_loss = total_loss / len(testloader.dataset)
+        return accuracy, avg_loss
+
+
+def main():
+    save_model_every_n_epochs = save_model_every
+    trainloader, testloader, _ = prepare_data(batch_size=batch_size)
+    model = ViTForClassfication(config)
+    optimizer = optim.AdamW(model.parameters(), lr=lr, weight_decay=1e-2)
+    loss_fn = nn.CrossEntropyLoss()
+    trainer = Trainer(model, optimizer, loss_fn, exp_name, device=device)
+    trainer.train(trainloader, testloader, epochs, save_model_every_n_epochs=save_model_every_n_epochs)
+
+
+if __name__ == '__main__':
+    main()
